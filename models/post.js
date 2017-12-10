53a314bcd8fe2d836855e79c696c867d0e59d8aa
Ryosaku <hiwaryo@gmail.com>
2017-12-10 12:11:53 -0800

    better

--- models/post.js
+++ models/post.js
@@ -17,5 +17,16 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: "Personal"
     }
   });
+
+  Post.associate = function(models) {
+    // We're saying that a Recipe should belong to an Author
+    // A Recipe can't be created without an Author due to the foreign key constraint
+    Post.belongsTo(models.Author, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
   return Post;
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,5 +25,10 @@ module.exports = function(app) {
   app.get("/blog", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/blog.html"));
   });
+  
+  // authors route loads author-manager.html
+  app.get("/authors", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+  });
 
 };
