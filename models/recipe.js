2fcad7a66086370ffc8d61f5af3fad6fa1ce7b2e
Ryosaku <hiwaryo@gmail.com>
2018-01-02 21:22:11 -0800

    fixed like and dislike function

--- models/recipe.js
+++ models/recipe.js
@@ -66,7 +66,7 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     image: {
-      type: DataTypes.BLOB,
+      type: DataTypes.STRING,
       allowNull: true,
       validate: {
         len: [1]
